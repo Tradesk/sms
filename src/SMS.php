50e2f6f2517a669fc37bdd962023efb51db2c78f
sam_roksta <mbuthias21@gmail.com>
2020-12-15 18:17:12 +0300

    fixed exnpoint

--- src/SMS.php
+++ src/SMS.php
@@ -9,7 +9,7 @@ use GuzzleHttp\Client;
 class SMS
 {
     public $app_key, $app_secret, $message;
-    public $server_api = 'http://localhost:8000';
+    public $server_api = 'http://api.tradesk.co.ke';
     public $receipients = [];
 
     function __construct($app_key, $app_secret)
